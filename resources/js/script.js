0edf59032bb88d00bd89a01a258c1c6ad53467eb
Valerio-Catellani <valeriocatellani@gmail.com>
2024-03-27 18:40:08 +0100

    created simon's says for test

--- resources/js/script.js
+++ resources/js/script.js
@@ -32,5 +32,63 @@ const app = createApp({
     }
 
 });
-
 app.mount('#app');
+
+
+
+
+
+const app2 = createApp({
+    data() {
+        return {
+            title: "Simon Says",
+            subtTitle: "After 15 seconds, write the numbers you rememeber in the circles",
+            randomNumbers: [],
+            randomNumbersTmp: [],
+            gameIsRunning: false,
+            isInputDisabled: true,
+            checkResults: false,
+        }
+    },
+    methods: {
+        createNumbers() {
+            this.gameIsRunning = true;
+            this.checkResults = false;
+            this.randomNumbers = [];
+            this.randomNumbersTmp = [];
+            this.isButtonDisabled = true;
+            console.log(this);
+            for (let i = 0; i < 5; i++) {
+                this.randomNumbersTmp.push(this.getRndInteger(1, 100));
+            };
+            this.randomNumbers = this.randomNumbersTmp;
+            this.hideAll()
+        },
+        getRndInteger(min, max) {
+            return Math.floor(Math.random() * (max - min + 1)) + min;
+        },
+        hideAll() {
+            setTimeout(() => {
+                this.randomNumbersTmp = [];
+                console.log(this.randomNumbers);
+                this.isButtonDisabled = false;
+                this.isInputDisabled = false;
+            }, 15000)
+        },
+        changeColor(index) {
+            if (this.checkResults && this.randomNumbersTmp[index] === this.randomNumbers[index]) {
+                return 'bg-success'
+            } else if (this.checkResults && this.randomNumbersTmp[index] !== this.randomNumbers[index]) {
+                return 'bg-danger'
+            } else {
+                return 'bg-dark-subtle'
+            }
+        },
+        confrontResults() {
+            this.checkResults = true;
+            this.isInputDisabled = true;
+        }
+
+    }
+})
+app2.mount('#app2');
